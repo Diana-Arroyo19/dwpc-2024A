68c384ee18de14017c5cb79b0f1862ef9e738ed6
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2024-02-20 04:37:21 +0000

    🌟Enhancement: Babel en el front-end Beneficios de programar con lo ultimo de JavaScript, incluir a Babel en webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,41 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     // 2.2 Nombre del archivo de salida
     filename: "bundle.js",
-		// 2.3 Ruta base de archivos estáticos
+    // 2.3 Ruta base de archivos estáticos
 		publicPath: "/"
   },
-}
+  // 3. Configuración de los loaders
+  module:{
+    rules: [
+      // 3.1 Reglas para archivos JS
+      {
+        // 3.1.1 Expresión regular para identificar archivos
+        test: /\.js$/,
+        // 3.1.2 Excluir archivos de la carpeta node_modules
+        exclude: /node_modules/,
+        // 3.1.3 Usar el loader de babel
+        use:[
+          {
+            loader: "babel-loader",
+            // 3.1.4 Opciones de configuración de babel
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  // 3.1.5 Opciones de configuración de preset-env 
+                  {
+                    "modules": false,
+                    "useBuiltIns": "usage",
+                    // 3.1.6 Corejs para usar con polyfills
+                    "targets": '> 0.25%, not dead',
+                    "corejs": 3
+                  }
+                ],
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -23,7 +23,7 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     // 2.2 Nombre del archivo de salida
     filename: "bundle.js",
-		// 2.3 Ruta base de archivos estáticos
+    // 2.3 Ruta base de archivos estáticos
 		publicPath: "/"
   },
   // 3. Configurando el servidor de desarrollo
@@ -37,5 +37,39 @@ module.exports = {
     port: 8080,
     // 3.3 Definiendo el host
     host: "0.0.0.0"
+  },
+  // 3. Configuración de los loaders
+  module:{
+    rules: [
+      // 3.1 Reglas para archivos JS
+      {
+        // 3.1.1 Expresión regular para identificar archivos
+        test: /\.js$/,
+        // 3.1.2 Excluir archivos de la carpeta node_modules
+        exclude: /node_modules/,
+        // 3.1.3 Usar el loader de babel
+        use:[
+          {
+            loader: "babel-loader",
+            // 3.1.4 Opciones de configuración de babel
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  // 3.1.5 Opciones de configuración de preset-env 
+                  {
+                    "modules": false,
+                    "useBuiltIns": "usage",
+                    // 3.1.6 Corejs para usar con polyfills
+                    "targets": '> 0.25%, not dead',
+                    "corejs": 3
+                  }
+                ],
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
-}
+};
