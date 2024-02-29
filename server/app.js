74ace84e8c28abd2b13b525e065bdad7f24de56f
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2024-02-29 21:13:53 +0000

    🌟Enhancement: Webpack middleware El servidor de desarrollo de webpack proporciona el script principal y los estilos requeridos por nuestra aplicación del lado del cliente a todas las páginas que carga el navegador.

--- server/app.js
+++ server/app.js
@@ -13,7 +13,46 @@ import logger from 'morgan';
 import usersRouter from'./routes/users';
 import indexRouter from'./routes/index';
 
+// Importando las dependencias de webpack
+import webpack from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+
+// Importando configuracion de webpack
+import webpackConfig from '../webpack.dev.config';
+
+// Creando la intancia express
 var app = express();
+
+// Obteniendo el modo de ejecucion de la app
+const nodeEnviroment = process.env.NODE_ENV || 'production' 
+
+// Cinfigurando entorno de desarrollo
+if(nodeEnviroment === 'developement'){
+  console.log("🛠️ Ejecutando en modo desarrollo 🛠️");
+  // Agregando el modo de ejecucion a la configuracion
+  webpackConfig.mode = 'development';
+  // Estableciendo el valor del puerto del servidor de desarrollo
+  webpackConfig.devServer.port = process.env.PORT;
+  // Configurando el HMR (Hot Module Replacement)
+  webpackConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackConfig.entry
+  ];
+	// Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Generando el empquetador (bundle) de webpack
+  const bundle = webpack(webpackConfig);
+  // Habilitando el webpack middleware
+  app.use( WebpackDevMiddleware(bundle, {
+    publicPath: webpackConfig.output.publicPath
+  }) );
+  //  Agregando el midelware webpack HMR
+  app.use( WebpackHotMiddleware(bundle) );
+}else{
+  console.log("🏭 Ejecutando en modo producción 🏭");
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
