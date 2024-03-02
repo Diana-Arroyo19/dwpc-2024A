fee3c39de4a7ab1bd1d4b9a002e9266f7c4cc6fd
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2024-03-02 04:09:12 +0000

    ✔📄Fix and🌟Enhancement: VsCode Auto Fix Corrigiendo errores Visual Studio Code puede reparar automáticamente aquellos errores que no requieren intervención del programador cada vez que guardamos el archivo.

--- server/app.js
+++ server/app.js
@@ -3,55 +3,56 @@
 import createError from 'http-errors';
 // Ayuda a crear servidores web
 import express from 'express';
-//Nucleo de node, ayuda al manejo de las rutas
+// Nucleo de node, ayuda al manejo de las rutas
 import path from 'path';
 // Ayuda al manejo de las cookies
 import cookieParser from 'cookie-parser';
 // Maneja el log de las peticiones http
 import logger from 'morgan';
 
-import usersRouter from'./routes/users';
-import indexRouter from'./routes/index';
-
 // Importando las dependencias de webpack
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
 // Importando configuracion de webpack
 import webpackConfig from '../webpack.dev.config';
 
 // Creando la intancia express
-var app = express();
+const app = express();
 
 // Obteniendo el modo de ejecucion de la app
-const nodeEnviroment = process.env.NODE_ENV || 'production' 
+const nodeEnviroment = process.env.NODE_ENV || 'production';
 
 // Cinfigurando entorno de desarrollo
-if(nodeEnviroment === 'developement'){
-  console.log("🛠️ Ejecutando en modo desarrollo 🛠️");
+if (nodeEnviroment === 'developement') {
+  console.log('🛠️ Ejecutando en modo desarrollo 🛠️');
   // Agregando el modo de ejecucion a la configuracion
   webpackConfig.mode = 'development';
   // Estableciendo el valor del puerto del servidor de desarrollo
   webpackConfig.devServer.port = process.env.PORT;
   // Configurando el HMR (Hot Module Replacement)
   webpackConfig.entry = [
-    "webpack-hot-middleware/client?reload=true&timeout=1000",
-    webpackConfig.entry
+    'webpack-hot-middleware/client?reload=true&timeout=1000',
+    webpackConfig.entry,
   ];
-	// Agregar el plugin a la configuración de desarrollo
+  // Agregar el plugin a la configuración de desarrollo
   // de webpack
   webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
   // Generando el empquetador (bundle) de webpack
   const bundle = webpack(webpackConfig);
   // Habilitando el webpack middleware
-  app.use( WebpackDevMiddleware(bundle, {
-    publicPath: webpackConfig.output.publicPath
-  }) );
+  app.use(
+    WebpackDevMiddleware(bundle, {
+      publicPath: webpackConfig.output.publicPath,
+    }),
+  );
   //  Agregando el midelware webpack HMR
-  app.use( WebpackHotMiddleware(bundle) );
-}else{
-  console.log("🏭 Ejecutando en modo producción 🏭");
+  app.use(WebpackHotMiddleware(bundle));
+} else {
+  console.log('🏭 Ejecutando en modo producción 🏭');
 }
 
 // view engine setup
@@ -70,12 +71,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+
 const router = express.Router();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express with Babel' });
 });
 
-module.exports = router;
+export default router;
